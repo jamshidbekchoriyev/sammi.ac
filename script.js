c53879bd9f79b42ada9618c86d831099aa342178
Jamshid <jamshidbekchoriyev70@gmail.com>
2024-10-20 22:40:07 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict"
+
+// const age = +prompt("How old are you?", "22");
+// console.log(age)
+
+// interpolatsiya
+// const channel = prompt("what's your favourite youtuber?")
+// console.log(`https://youtube.com/ ${channel}`)
+
+// shartli operatorlar
+// if(age > 25) {
+//     console.log("Horror films")
+// } else{
+//     console.log("cartoon")
+// }
+
+// age > 25 ? console.log("Horror films") : console.log("cartoon");
+
+const color = "black";
+switch(color){
+    case "red":
+        console.log("stop")
+    break;
+    case "yellow":
+        console.log("prepare")
+    break;
+    case "green":
+        console.log("GO")
+    break;
+        default: console.log("go to doctor daltonik")
+    break;
+}
