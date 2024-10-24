96bb3689b81fdbed044a3c83b2e8ece49da0477f
Jamshid <jamshidbekchoriyev70@gmail.com>
2024-10-24 14:31:36 +0900

    amaliyot

--- script.js
+++ script.js
@@ -16,17 +16,37 @@
 
 // age > 25 ? console.log("Horror films") : console.log("cartoon");
 
-const color = "black";
-switch(color){
-    case "red":
-        console.log("stop")
-    break;
-    case "yellow":
-        console.log("prepare")
-    break;
-    case "green":
-        console.log("GO")
-    break;
-        default: console.log("go to doctor daltonik")
-    break;
-}
+// const color = "black";
+// switch(color){
+//     case "red":
+//         console.log("stop")
+//     break;
+//     case "yellow":
+//         console.log("prepare")
+//     break;
+//     case "green":
+//         console.log("GO")
+//     break;
+//         default: console.log("go to doctor daltonik")
+//     break;
+// }
+
+
+// amaliyot
+const numberOfSeries = +prompt("Nechta serial ko'rdingiz?", "");
+const SeriesDb = {
+    count: numberOfSeries,
+    series: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
+const a = prompt("Oxirgi ko'rgan serialingiz?"),
+    b = prompt("Nechi baho berasiz?"),
+    c = prompt("Oxirgi ko'rgan serialingiz?"),
+    d = prompt("Nechi baho berasiz?");
+
+SeriesDb.series.a = b;
+SeriesDb.series.c = d;
+
+console.log(numberOfSeries)
